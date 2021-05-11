7b3e79e5fd409db146fb59822a6149ea967bd19f
douglasdossantos22 <douglasdossantos22@gmail.com>
2021-05-11 13:34:30 -0300

    Ajustes Mysql

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,16 +1,12 @@
-CREATE DATABASE `modulos` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE IF NOT EXISTS `modulos` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE `cursos` (
+CREATE TABLE IF NOT EXISTS `cursos` (
   `cursoId` int(11) NOT NULL,
   `cursoName` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`cursoId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO modulos.cursos (cursoId,cursoName) VALUES
-	 (8,'Docker2'),
-	 (9,'Kubernetes2'),
-	 (10,'Integração Contínua2'),
-	 (11,'Git e GitHub2'),
-	 (12,'Service Mesh com Istio2'),
-	 (13,'Comunicação2'),
-	 (14,'DDD e Arquitetura Hexagonal2');
+	 (8,'Curso Teste seed 01'),
+	 (9,'Curso Teste seed 02'),
+	 (10,'Curso Teste seed 03');
